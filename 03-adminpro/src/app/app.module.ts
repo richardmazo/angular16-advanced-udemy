9fc03a016702d914c29c006bf75afa0b02fdaa7b
Richard <richard.mazo.97@hotmail.com>
2024-01-02 08:57:28 -0500

    Se inicia curso avanzado angular

---
+++ 03-adminpro/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BreadcumbsComponent } from './shared/breadcumbs/breadcumbs.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    BreadcumbsComponent,
+    SidebarComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ 03-adminpro/src/app/shared/breadcumbs/breadcumbs.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-breadcumbs',
+  templateUrl: './breadcumbs.component.html',
+  styles: [
+  ]
+})
+export class BreadcumbsComponent {
+
+}
