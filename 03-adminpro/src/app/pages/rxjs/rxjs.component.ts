a28f3c485b3e67f04b0cdf9634e9cd2fa403e056
Richard <richard.mazo.97@hotmail.com>
2024-01-10 11:08:48 -0500

    Fin seccion 8

--- 03-adminpro/src/app/pages/rxjs/rxjs.component.ts
+++ 03-adminpro/src/app/pages/rxjs/rxjs.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Observable, Subscription, interval } from 'rxjs';
+import { retry, take, map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-rxjs',
@@ -6,6 +8,54 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class RxjsComponent {
+export class RxjsComponent implements OnDestroy {
+
+  public intervalSubs: Subscription;
+
+  constructor(){
+
+   /*this.retornaObservable().pipe(
+      retry(1)
+    ).subscribe(
+      valor => console.log('Subs: ', valor),
+      error => console.warn('Error: ', error),
+      () => console.info('Obs terminado')
+    );*/
+
+    this.intervalSubs = this.retornaIntervalo()
+      .subscribe( console.log );
+
+  }
+
+  ngOnDestroy(): void {
+    this.intervalSubs.unsubscribe();
+  }
+
+  retornaIntervalo(): Observable<number>{
+    return interval(100).pipe(
+      //take(10),
+      map( valor => valor + 1 ),
+      filter( valor => valor % 2 === 0 ),
+    );
+  }
+
+  retornaObservable(): Observable<number> {
+    let i = -1;
+    return new Observable<number>( observer => {
+
+      const interval = setInterval( () => {
+        i++;
+        observer.next(i);
+        if(i === 4){
+          clearInterval(interval);
+          observer.complete();
+        }
+        if(i === 2){
+          observer.error('i llegó al valor de 2');
+        }
+      }, 1000);
+    
+    });
+  }
 
 }
--- 03-adminpro/src/app/shared/breadcumbs/breadcumbs.component.ts
+++ 03-adminpro/src/app/shared/breadcumbs/breadcumbs.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivatedRoute, ActivationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-breadcumbs',
@@ -6,6 +9,30 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class BreadcumbsComponent {
+export class BreadcumbsComponent implements OnDestroy {
+
+  public titulo?: string;
+  public tituloSubs$: Subscription;
+
+  constructor( private router: Router, private route: ActivatedRoute ) {
+    this.tituloSubs$ = this.getArgumentosRuta()
+      .subscribe( ({ titulo }) => {
+        this.titulo = titulo;
+        document.title = `AdminPro - ${ titulo }`;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.tituloSubs$.unsubscribe();
+  }
+
+  getArgumentosRuta(){
+    return this.router.events
+    .pipe(
+      filter((event): event is ActivationEnd => event instanceof ActivationEnd),
+      filter((event:ActivationEnd) => event.snapshot.firstChild === null ),
+      map( (event: ActivationEnd) => event.snapshot.data )
+    );
+  }
 
 }
