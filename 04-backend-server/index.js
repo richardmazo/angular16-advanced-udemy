ba231bf3d01c7226ee4cf328116654146194dae5
Richard <richard.mazo.97@hotmail.com>
2024-01-14 16:57:36 -0500

    Finalizando seccion 10

--- 04-backend-server/index.js
+++ 04-backend-server/index.js
@@ -9,20 +9,18 @@ const app = express();
 //Configurar CORS
 app.use(cors());
 
+//Lectura y parseo del body
+app.use(express.json());
+
 //Base de datos
 dbConnection();
 
 //Rutas
-app.get('/', (req, res) => {
-    res.json({
-        ok: true,
-        msg: 'Hola Mundo'
-    });
-});
+app.use('/api/usuarios', require('./routes/usuarios'));
+app.use('/api/login', require('./routes/auth'));
 
 app.listen(process.env.PORT, () => {
     console.log('Server is running on port 3000');
 })
 
-///richardmazo
-//1qaZ2wsX3edC4r*
+
