ba425e4327c9d726fb6395e37501f7cad51e2f80
Richard <richard.mazo.97@hotmail.com>
2024-01-15 17:30:19 -0500

    Continuacion seccion 11

---
+++ 04-backend-server/routes/hospitales.js
@@ -0,0 +1,35 @@
+/*
+    Hospitales
+    Ruta: /api/hospitales 
+ */
+
+const { Router } = require('express');
+
+const router = Router();
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
+
+const { getHospitales, crearHospital, actualizarHospital, borrarHospital} = require('../controllers/hospitales');
+const { validarJWT } = require('../middlewares/validar-jwt');
+
+router.get('/', getHospitales);
+
+router.post('/', 
+    [
+        validarJWT,
+        check('nombre', 'El nombre del hospital es necesario').not().isEmpty(),
+        validarCampos
+    ],
+    crearHospital
+);
+
+router.put('/:id',
+    [],
+    actualizarHospital
+);    
+
+router.delete('/:id',
+    borrarHospital
+);  
+
+module.exports = router;
---
+++ 04-backend-server/routes/medicos.js
@@ -0,0 +1,36 @@
+/*
+    Medicos
+    Ruta: /api/medicos 
+*/
+
+const { Router } = require('express');
+
+const router = Router();
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
+
+const { getMedicos, crearMedico, actualizarMedico, borrarMedico } = require('../controllers/medicos');
+const { validarJWT } = require('../middlewares/validar-jwt');
+
+router.get('/', getMedicos);
+
+router.post('/', 
+    [
+        validarJWT,
+        check('nombre', 'El nombre del médico es necesario').not().isEmpty(),
+        check('hospital', 'El hospital id debe de ser válido').isMongoId(),
+        validarCampos
+    ],
+    crearMedico
+);
+
+router.put('/:id',
+    [],
+    actualizarMedico
+);    
+
+router.delete('/:id',
+    borrarMedico
+);  
+
+module.exports = router;    
