8bd620b2d57e1b7053fe0e88632e34ffc74141c4
Richard <richard.mazo.97@hotmail.com>
2024-02-08 09:56:19 -0500

    Finalizando seccion 19

--- 03-adminpro/src/app/pages/pages.module.ts
+++ 03-adminpro/src/app/pages/pages.module.ts
@@ -9,10 +9,10 @@ import { ComponentsModule } from '../components/components.module';
 
 import { PipesModule } from '../pipes/pipes.module';
 
+import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
-import { PagesComponent } from './pages.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
 import { PromesasComponent } from './promesas/promesas.component';
 import { RxjsComponent } from './rxjs/rxjs.component';
--- 04-backend-server/index.js
+++ 04-backend-server/index.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+const path = require('path');
+
 const express = require('express');
 const cors = require('cors');
 
@@ -25,6 +27,11 @@ app.use('/api/medicos', require('./routes/medicos'));
 app.use('/api/todo', require('./routes/busquedas'));
 app.use('/api/login', require('./routes/auth'));
 app.use('/api/upload', require('./routes/uploads'));
+
+//Lo último
+/*app.get('*', (req, res) => {
+    res.sendFile( path.resolve( __dirname, 'public/index.html' ) )
+})*/
 
 app.listen(process.env.PORT, () => {
     console.log('Server is running on port 3000');
