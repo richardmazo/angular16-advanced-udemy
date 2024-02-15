86621c9ee44792486360effa08233306e5beb8fa
Richard <richard.mazo.97@hotmail.com>
2024-02-15 16:10:02 -0500

    Finalizando seccion 24

---
+++ 06-pruebas/src/app/intermedio2/medico/medico.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { MedicoService } from './medico.service';
+
+@Component({
+  selector: 'app-medico',
+  templateUrl: './medico.component.html',
+  styles: []
+})
+export class MedicoComponent {
+
+  medicos: any[] = [];
+
+  constructor( public _medicoService: MedicoService ){ }
+
+  saludarMedico( nombre: string ){
+    return `Hola ${ nombre }`;
+  }
+
+  obtenerMedicos() {
+
+    this._medicoService.getMedicos()
+        .subscribe( (medicos: any ) => this.medicos = medicos );
+
+  }
+
+}
